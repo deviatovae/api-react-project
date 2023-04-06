887007a67c1a0fe7cdb1d42d510de2260c70d05f
Alena Deviatova <aedeviatova@yandex.ru>
2023-04-06 15:31:58 +0300

    feat: filter by all cards fields, add endpoint to get single card

--- src/controller/cards.ts
+++ src/controller/cards.ts
@@ -1,5 +1,5 @@
 import { IncomingMessage, ServerResponse } from 'http';
-import data from '../data/cards.json'
+import data from '../data/cards.json';
 import * as url from 'url';
 
 export default class Cards {
@@ -7,9 +7,34 @@ export default class Cards {
     const query = url.parse(req.url, true).query
 
     const { q } = query;
-    const result = typeof q === 'string' ? data.filter((card) => card.name.toLowerCase().includes(q.toLowerCase())) : data
+    const result = typeof q === 'string' ? data.filter((card) => {
+      const fields = [card.name, card.time, card.price, card.description]
+      for (const field of fields) {
+        if (field.toString().toLowerCase().includes(q.toLowerCase())) {
+          return true
+        }
+      }
+      return false
+    }) : data
 
     res.writeHead(200);
     res.end(JSON.stringify(result));
+  }
+  getCard(id: string, res: ServerResponse) {
+    const idNum = parseInt(id)
+    if (isNaN(idNum)) {
+      res.writeHead(404)
+      res.end();
+      return
+    }
+
+    const card = data.find(({ id: cardId }) => cardId === idNum)
+    if (!card) {
+      res.writeHead(404)
+      res.end();
+      return
+    }
+    res.writeHead(200);
+    res.end(JSON.stringify(card));
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,23 @@ const port = parseInt(process.env.PORT) || 8080;
 const requestListener = function (req: IncomingMessage, res: ServerResponse) {
   res.setHeader('Content-Type', 'application/json');
   const cards = new Cards()
-  const reqUrl = url.parse(req.url, false)
+  const reqUrl = url.parse(req.url, false).pathname
 
-  switch (reqUrl.pathname) {
-    case '/cards':
-      cards.getCards(req, res)
-      break
-    default:
-      res.writeHead(404)
-      res.end('Not found')
+  if (reqUrl === '/cards') {
+    cards.getCards(req, res)
+    return
   }
+
+  const cardRoute = '/cards/'
+  const cardIdStart = reqUrl.indexOf(cardRoute)
+  if (cardIdStart >= 0) {
+    const id = reqUrl.substring(cardRoute.length)
+    cards.getCard(id, res)
+    return
+  }
+
+  res.writeHead(404)
+  res.end('Not found')
 };
 
 const server = createServer(requestListener);
